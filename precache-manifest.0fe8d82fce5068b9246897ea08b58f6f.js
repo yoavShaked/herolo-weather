17ebe8149cf1a3a654c7c211bba6926fbd77b537
Yoav Shaked <yoavcad@gmail.com>
2020-06-02 17:38:31 +0300

    Updates

---
+++ precache-manifest.0fe8d82fce5068b9246897ea08b58f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1382577e192b39e8b3be2c66a6e4d1d2",
+    "url": "/herolo-weather/index.html"
+  },
+  {
+    "revision": "6ad63daed0bb80205451",
+    "url": "/herolo-weather/static/css/main.59414811.chunk.css"
+  },
+  {
+    "revision": "c37c01f7fced3c5fdb10",
+    "url": "/herolo-weather/static/js/2.afd993f7.chunk.js"
+  },
+  {
+    "revision": "c8fda1946ee89962b7e3857300fba486",
+    "url": "/herolo-weather/static/js/2.afd993f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad63daed0bb80205451",
+    "url": "/herolo-weather/static/js/main.78530290.chunk.js"
+  },
+  {
+    "revision": "8c480d657b71dcbdb6d3",
+    "url": "/herolo-weather/static/js/runtime-main.c11780cc.js"
+  }
+]);
