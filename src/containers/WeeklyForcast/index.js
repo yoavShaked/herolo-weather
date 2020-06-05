50ffb4eb1bcda6283f996a623ac43f4ac941ea30
Yoav Shaked <yoavcad@gmail.com>
2020-06-05 23:04:57 +0300

    put map weather out of scope of Weekly forcast function

--- src/containers/WeeklyForcast/index.js
+++ src/containers/WeeklyForcast/index.js
@@ -3,33 +3,33 @@ import { connect } from "react-redux";
 
 import { get } from "lodash/fp";
 import { Typography, Tooltip } from "@material-ui/core";
-import icons from '../../icons';
+import icons from "../../icons";
 import Icon from "../../components/Icon";
 import { WeatherItemContainer, Container } from "./styled-components";
 
+const mapWeatherItem = (dayTime) => (weather, index) => {
+  const minTemperature = get(["temperature", "min"], weather);
+  const maxTemperature = get(["temperature", "max"], weather);
+  const description = get([dayTime, "IconPhrase"], weather);
+  const iconId = get([dayTime, "Icon"], weather);
+  const iconSrc = get(iconId, icons);
+
+  return (
+    <WeatherItemContainer
+      flexDirection="column"
+      key={`${index}-${minTemperature}-${maxTemperature}`}
+    >
+      <Icon src={iconSrc} alt="" />
+      <Tooltip title={description}>
+        <Typography className="description">{description}</Typography>
+      </Tooltip>
+      <Typography>{`${minTemperature} / ${maxTemperature}`}</Typography>
+    </WeatherItemContainer>
+  );
+};
+
 const WeeklyForcast = ({ weatherForcast, dayTime }) => {
-  const mapWeather = (weather, index) => {
-    const minTemperature = get(["temperature", "min"], weather);
-    const maxTemperature = get(["temperature", "max"], weather);
-    const description = get([dayTime, "IconPhrase"], weather);
-    const iconId = get([dayTime, "Icon"], weather);
-    const iconSrc = get(iconId, icons);
-
-    return (
-      <WeatherItemContainer
-        flexDirection="column"
-        key={`${index}-${minTemperature}-${maxTemperature}`}
-      >
-        <Icon src={iconSrc} alt="" />
-        <Tooltip title={description}>
-          <Typography className="description">{description}</Typography>
-        </Tooltip>
-        <Typography>{`${minTemperature} / ${maxTemperature}`}</Typography>
-      </WeatherItemContainer>
-    );
-  };
-
-  return <Container>{weatherForcast.map(mapWeather)}</Container>;
+  return <Container>{weatherForcast.map(mapWeatherItem(dayTime))}</Container>;
 };
 
 const mapStateToProps = (state) => ({
